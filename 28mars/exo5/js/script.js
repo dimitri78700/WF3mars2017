d617255fd24fb726d6fa7fe7c22468af2a969de7
Etudiant <Etudiant>
2017-03-28 11:35:17 +0200

    Javascript Début

--- 28mars/exo5/js/script.js
+++ 28mars/exo5/js/script.js
@@ -22,3 +22,24 @@ console.log(MyConc);
 // Opérateur de soustraction : -
 var mySous = 7 - 5;
 console.log(mySous);
+
+// Opérateur de multiplication : *
+var myMult = 7*5;
+console.log(myMult);
+
+// '''  de division : /
+var myDivi = 14 / 3;
+console.log(myDivi);
+
+
+// L'opérateur modulo : %
+var myModulo = 12 % 5;
+console.log(myModulo);
+
+// Opérateur d'addition et d'affectation : +=
+var a = 10;
+var b = 5;
+
+// a = a + b;
+a += b;
+console.log(a);
