ee603e6ec2ccc46d5c4b54cde4eba6d0c0c6460c
Etudiant <Etudiant>
2017-03-28 11:22:41 +0200

    Javascript début

---
+++ 28mars/exo5/js/script.js
@@ -0,0 +1,24 @@
+// Créer une variable de type NUMBER avec la valeur 10
+
+// Opérateur d'affectation : =
+var myNumber = 10;
+console.log(myNumber);
+
+// Opérateur d'incrémentation : ++
+myNumber++;
+console.log(myNumber);
+
+// Opérateur de décrementation: --
+myNumber--;
+console.log(myNumber);
+
+// Opérateur d'addition/concaténation : +
+var myAdd = 7 + 5;
+console.log(myAdd);
+
+var MyConc = 'Salut ' + 'mon ' + 'pote ';
+console.log(MyConc);
+
+// Opérateur de soustraction : -
+var mySous = 7 - 5;
+console.log(mySous);
