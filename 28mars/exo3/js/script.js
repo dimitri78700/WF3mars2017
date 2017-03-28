f437e6da8d5511f88713df3fbebfd89cbd0b13fe
Etudiant <Etudiant>
2017-03-28 10:47:12 +0200

    Javascript

---
+++ 28mars/exo3/js/script.js
@@ -0,0 +1,14 @@
+// toujours mettre un point virgule à la fin
+// Créer une variable de type STRING   
+ 
+var myString = 'Je suis une string';
+console.log(myString);
+
+// Créer une var de type NUMBER (INTEGER)
+var myNumber = 237;
+console.log(myNumber);
+
+// Créer une variable de type BOOLEAN : true/false
+var myBoolean = true;
+console.log(myBoolean);
+
---
+++ 28mars/exo4/js/script.js
@@ -0,0 +1,5 @@
+// Créer une variable pour demander le nom de l'utilisateur 
+var userName = prompt('Quel est ton nom ? ');
+
+// afficher dans la console un message de Welcome
+console.log('Bonjour ' + userName);
