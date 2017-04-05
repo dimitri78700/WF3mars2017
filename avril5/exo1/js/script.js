0c06ca9f7b4b63543acbcd55657402a07460d3ae
Etudiant <Etudiant>
2017-04-05 12:47:03 +0200

    avril 5 modal() formulaire

--- avril5/exo1/js/script.js
+++ avril5/exo1/js/script.js
@@ -46,14 +46,60 @@ $(document).ready(function(){
          // capter la soumission du formulaire
             $('form').submit(function(evt){
 
+        // Créer une variable pour la validation finale du formulaire
+            var formScore = 0;
+
             // Bloquer le comportement par default du formulaire
             evt.preventDefault();
 
-            console.log('submit du form')
-    });
+            // Minimum 4 caractéres pour l'email et 5 caractéres pour le message
+            
+                if( $('[type="email"]').val().length < 4 
+                ){
+
+                    console.log('Email Manquant');
+
+                } else{
+                    console.log('email OK')
+
+                    // incrémenter formScore
+                    formScore++;
+                };
+
+           // Minimum 5 caractéres pour le message
+            
+                if( $('textarea').val().length < 5
+                ){
+
+                    console.log('Mess Manquant');
+
+                } else{
+                    console.log('message OK')
+
+                    // incrémenter formScore
+                    formScore++;
+                };
+
+           // verifier la valeur du formulaire
+                if( formScore == 2){
+                    console.log('le form est bon')
+
+                    // => envoi des données vers le fichier de traitement PHP
+                        // => le fichier PHP répondu true : je peux continuer mon code
+
+                        // Ajouter le message dans la balise aside
+                        $('aside').append('<h3>' + $('textarea').val() + '</h3><p>' + $('[type="email"]').val() + '</p>');
 
 
-    }
+                        // reset du formulaire
+                        $('form')[0].reset();
+
+                };
+
+      });
+
+
+    };
 
 
 });
