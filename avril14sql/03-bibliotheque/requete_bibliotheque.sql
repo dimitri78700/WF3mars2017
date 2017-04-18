e2b83bddc10b559ed01f1c7e7873b17b6d2d22e7
Etudiant <Etudiant>
2017-04-18 14:01:35 +0200

    avril18sql

--- avril14sql/03-bibliotheque/requete_bibliotheque.sql
+++ avril14sql/03-bibliotheque/requete_bibliotheque.sql
@@ -79,3 +79,67 @@ USE bibliotheque;
     -- Exercices : Afficher le ou les titres de livre que Chloé n'a pas encore rendu ?
 
     SELECT titre FROM livre WHERE id_livre IN ( SELECT id_livre FROM emprunt WHERE date_rendu IS NULL AND id_abonne IN (SELECT id_abonne FROM abonne WHERE prenom = 'Chloé'));
+
+    -- Exercices : Combien de livres Benoit a empruntés ? 
+
+    SELECT COUNT(id_livre) FROM emprunt WHERE id_abonne IN ( SELECT id_abonne FROM abonne WHERE prenom ='Benoit' );
+
+    -- Exercices : Qui (prénom) a emprunté le plus de livres ? 
+
+    SELECT prenom FROM abonne WHERE id_abonne = ( SELECT id_abonne FROM emprunt GROUP BY id_abonne ORDER BY COUNT(id_emprunt) DESC LIMIT 0,1);   --  remarque : on ne peut pas utiliser LIMIT dans IN mais obligatoirement un '='
+
+
+
+
+
+-- **************************************
+-- Jointures internes
+-- **************************************
+
+    -- Différence entre jointure et une requête imbiriquée : un requête imbriquée est possible seulement dans le cas où les champs affichés ( ceux qui sont dans le SELECT ) sont issus de la même table.
+
+
+    -- Avec une requête de jointure, les champs sélectionnés peuvent être issus de table différentes. Une jointure est une reqûete permettant de faire des relations entre les différentes tables afain d'avoir des colonnes associées quui ne forme qu'un seul resultat 
+
+    -- Afficher les dates de sortie et de rendu pour l'abonnée Guillaume :
+
+    SELECT a.prenom, e.date_sortie, e.date_rendu 
+    FROM abonne a 
+    INNER JOIN emprunt e 
+    ON a.id_abonne = e.id_abonne
+    WHERE a.prenom = 'guillaume';
+
+    -- 1e ligne : ce que je souhaite afficher 
+    -- 2e ligne : la 1ere table d'ou provinnenent les informations
+    -- 3e ligne : la seconde table d'où provinnent les infos
+    -- 4e ligne : la jointure qui lie les 2 tables avec le champs COMMUN
+    -- 5e ligne : la ou les conditions supplémentaire 
+
+    -- Nous aimerions connaitre les mouvements des livres ( titre, date de sortie et date de rendu ) ecrit par Alphonse Daudet :
+
+    SELECT l.titre, e.date_sortie, e.date_rendu
+    FROM livre l
+    INNER JOIN emprunt e 
+    ON l.id_livre = e.id_livre
+    WHERE l.auteur = 'Alphonse Daudet';
+
+    -- Qui a emprunté "une vie" sur l'année 2011 ?
+
+    SELECT a.prenom
+    FROM abonne a 
+    INNER JOIN emprunt e 
+    ON a.id_abonne = e.id_abonne
+    INNER JOIN livre l 
+    ON l.id_livre = e.id_livre
+    WHERE l.titre = 'une vie' AND date_sortie LIKE '2011%';
+
+
+    -- Afficher le nombre de livres emprunté chaque abonnée
+
+    SELECT a.prenom, COUNT(e.id_emprunt) AS nombre
+    FROM abonne a 
+    INNER JOIN emprunt e
+    ON a.id_abonne = e.id_abonne
+    GROUP BY a.prenom;
+
+
