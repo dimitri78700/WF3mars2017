1ecbaeab95ca6c2b6ea04efae0319805985863ff
Etudiant <Etudiant>
2017-05-03 09:57:44 +0200

    PHP 3mai

---
+++ PHP/08-site/boutique.php
@@ -0,0 +1,31 @@
+<?php 
+    require_once ('inc/init.inc.php');
+
+// -------------------------- Traitement ---------------------------- //
+
+    // 1- Affichage des catégories de vêtements :
+    
+
+
+
+
+
+// -------------------------- Affichage ---------------------------- //
+
+    require_once ('inc/init.inc.php');
+?>
+
+<div class="row">
+    <div class="col-md-3">
+        <?php echo $contenu_gauche; ?>
+    </div>
+
+    <div class="col-md-9">
+        <div class="row">
+        <?php echo $contenu_droite; ?>
+      </div>
+    </div>
+</div>
+
+<?php 
+    require_once ('inc/init.inc.php');
