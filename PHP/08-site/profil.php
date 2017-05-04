25d7a2e470a3e1a73a8a9743e5b09e66640e8511
Etudiant <Etudiant>
2017-05-04 17:31:35 +0200

    PHP 4mai

--- PHP/08-site/profil.php
+++ PHP/08-site/profil.php
@@ -1,6 +1,8 @@
 <?php
     require_once('inc/init.inc.php');
-        //-------------------- TRAITEMENT ---------------------- 
+
+//-------------------- TRAITEMENT ---------------------- 
+
             // Si visiteur nn connecté, on l'envoie vers connexion.php :
             if(!internauteEstConnecte()){
                 header('location:connexion.php'); // Nous l'invition à se connecter'
@@ -23,10 +25,31 @@
                 $contenu .= '<p>Votre code postal :' . $_SESSION['membre']['code_postal'] . '</p>';
                 $contenu .= '<p>Votre ville :' . $_SESSION['membre']['ville'] . '</p>';
             $contenu .= '</div>';            
-            
-            
-            
-        //-------------------- AFFICHAGE ---------------------- 
+
+
+// Exercice :
+    /*
+        1- Afficher le suivi des commandes du membre (s'il y en a) dans une liste ul et li : id_commande, date et état de la commande. S'il n'y en a pas, vous afficher "aucune commande en cours"
+
+        2- 
+    */
+
+      $pdo = new PDO('mysql:host=localhost;dbname=site', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+
+      $membre = $_SESSION['membre']['id_membre'];
+
+      $suivi = executeRequete("SELECT id_commande, date_enregistrement, etat FROM commande WHERE id_membre = '$membre'");
+       
+      echo '<ul>';
+        while($membre = $suivi->fetch(PDO::FETCH_ASSOC)){
+            echo '<li>' . $membre['id_commande'] . '</li>';
+            echo '<li>' . $membre['date_enregistrement'] . '</li>';
+            echo '<li>' . $membre['etat'] . '</li>';
+        }
+        echo '</ul>'; 
+
+ //-------------------- AFFICHAGE ---------------------- 
+
     require_once('inc/haut.inc.php');
     echo $contenu;
 ?>
