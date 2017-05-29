4c77514b0a2dee50684bb3cd5929613c54422c16
Dimitri.R <dimitriraievsky@icloud.com>
2017-05-29 17:14:59 +0200

    POO

--- POO/04_constante_static_self/Singleton.php
+++ POO/04_constante_static_self/Singleton.php
@@ -3,4 +3,6 @@
 // 04- Constante_static_self
     // Singleton.php
 
-// Design Pattern : Littéralement "Patron de conception", 
+// Design Pattern : Littéralement "Patron de conception", est une réponse donnée à un probléme que rencontre plusieurs (tous) développeurs. 
+
+// Singleton est la réponse à la question : Comment créer une classe qui ne sera instanciable qu'une seule et unique fois. 
